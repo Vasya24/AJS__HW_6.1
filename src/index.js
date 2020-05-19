1e8a2604dce44c9b2dd41122b5e3b9ac468404a1
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-05-19 16:55:18 +0300

    Let's try this one

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 function Character(name, type) {
-    this.name = name;
-    this.type = type;
-    
-    if (name.length < 2) {
-      throw Error('Слишком корокткое имя')
-    } else if (name.length > 10) {
-      throw Error('Слишком длинное имя')
-    };
-    if (typeof type != 'string') {
-      throw Error('Только строковое значение!')
-    }
-  }
-  
-  function NewChar(name, type) {
-    Character.call(this, name, type);
-    this.health = 100;
-    this.level = 1;
-    this.attack;
-    this.defence;
-    if (type === 'Bowman' || type === 'Undead') {
-      this.attack = 25;
-      this.defence = 25;
-    } else if (type === 'Swordsman' || type === 'Zombie') {
-      this.attack = 40;
-      this.defence = 10;
-    } else if (type === 'Magician' || type === 'Daemon') {
-      this.attack = 10;
-      this.defence = 40;
-  };
-    console.log(`Имя: ${this.name}\nТип: ${this.type}\nЗдоровье: ${this.health}\nУровень: ${this.level}\nАтака: ${this.attack}\nЗащита: ${this.defence}`)
-  }
-  
-  let sword = new NewChar('Базеилус', 'Swordsman');
+  this.name = name;
+  this.type = type;
+  this.health = 100;
+  this.level = 1;
+  this.attack;
+  this.defence;
+ 
+  // if (name.length < 2) {
+  //   throw new Error('Слишком корокткое имя')
+  // } else if (name.length > 10) {
+  //    throw Error('Слишком длинное имя')
+  // };
+//   if (typeof type != 'string') {
+//     throw new Error('Только строковое значение!')
+//   }
+// if (type === 'Bowman' || type === 'Undead') {
+//     this.attack = 25;
+//     this.defence = 25;
+//   } else if (type === 'Swordsman' || type === 'Zombie') {
+//      this.attack = 40;
+//      this.defence = 10;
+//   } else if (type === 'Magician' || type === 'Daemon') {
+//     this.attack = 10;
+//     this.defence = 40;
+//   };
+}
 
-  export {sword}
+export{ Character }
--- src/js/__tests__/index.test.js
+++ src/js/__tests__/index.test.js
@@ -1,5 +1,32 @@
-import {sword} from '../../index'
+import { Character } from '../../index'
 
-test('Test', () => {
-    expect(sword.attack).toBeGreaterThan(20)
+
+let vasya = new Character('Базелиус', 'Daemon');
+test('Name test', () => {
+    
+    if (vasya.name.length < 2) {
+        throw Error('Слишком короткое имя')
+      } else if (name.length > 10) {
+         throw Error('Слишком длинное имя')
+      };    
+})
+
+test('Type test', () => {
+    if (typeof vasya.type != 'string') {
+        throw Error('Только строковое значение!')
+      }
+    if (vasya.type === 'Bowman' || vasya.type === 'Undead') {
+        vasya.attack = 25;
+        vasya.defence = 25;
+      } else if (vasya.type === 'Swordsman' || vasya.type === 'Zombie') {
+         vasya.attack = 40;
+         vasya.defence = 10;
+      } else if (vasya.type === 'Magician' || vasya.type === 'Daemon') {
+        vasya.attack = 10;
+        vasya.defence = 40;
+      };
+})
+
+test('Final test', () => {
+expect(vasya.attack).toBeLessThan(vasya.defence)
 })
